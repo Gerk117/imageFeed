508f9866ff79d6653598c98f1589518286b469c4
George <george.xenodohov@yandex.ru>
2023-10-25 04:33:34 +0300

    hh

--- imageFeed/Constants.swift
+++ imageFeed/Constants.swift
@@ -7,12 +7,6 @@
 
 import Foundation
 
-//let accessKey = "3mjeZWAt5eVFvbRP06w67IS7r0IddxNsaWOkzpQ0HSY"
-//let secretKey = "REJzbqVDMt0kiYI9aDM_Wd7EXpND0B3WcvVKEtdbmHU"
-//let redirectURI = "urn:ietf:wg:oauth:2.0:oob"
-//let accessScope = "public+read_user+write_likes"
-//let defaultBaseURL = URL(string: "https://api.unsplash.com")
-
 enum AuthorisationConsts {
     static let accessKey = "3mjeZWAt5eVFvbRP06w67IS7r0IddxNsaWOkzpQ0HSY"
     static let secretKey = "REJzbqVDMt0kiYI9aDM_Wd7EXpND0B3WcvVKEtdbmHU"
